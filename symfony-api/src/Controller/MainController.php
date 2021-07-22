0c965e4323eac62001928711b73ed240e7be6204
Murod Khaydarov <murod.haydarov@gmail.com>
2021-07-22 13:33:42 +0300

    test validation

--- symfony-api/src/Controller/MainController.php
+++ symfony-api/src/Controller/MainController.php
@@ -2,14 +2,27 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Validator\Constraints\AbstractComparison;
+use Symfony\Component\Validator\Constraints\EqualTo;
+use Symfony\Component\Validator\Constraints\IdenticalTo;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\NotEqualTo;
+use Symfony\Component\Validator\Constraints\NotIdenticalTo;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 use Symfony\Contracts\Translation\TranslatorInterface;
 
 class MainController extends AbstractController
 {
-    public function index(TranslatorInterface $translator): JsonResponse
+    public function index(TranslatorInterface $translator, ValidatorInterface $validator): JsonResponse
     {
+        $user = new User(1, 'Bob');
+        $errors = $validator->validate($user, [
+            new IdenticalTo("Bob"),
+        ]);
+
         return $this->json([
             'message' => sprintf(
                 '%s %s %s',
---
+++ symfony-api/src/Entity/User.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Entity;
+
+class User
+{
+    /**
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $name;
+
+    public function __construct(int $id, string $name)
+    {
+        $this->id = $id;
+        $this->name = $name;
+    }
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+}
